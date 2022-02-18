5bf2ff82d50c09d3b06d2281bb01d263c3226232
EzyhB <ezyhbmail@gmail.com>
2022-02-18 23:00:56 +0000

    front end imported from week 9 project 1

--- src/services/EventApi.js
+++ src/services/EventApi.js
@@ -1,13 +1,11 @@
-const url = '';
-
 function updateDescription(id, description) {
-    fetch("http://localhost:5500/api/v1/events/" + id, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({event_description: description}),
-    });
+  fetch("http://localhost:5500/api/v1/events/" + id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ event_description: description }),
+  });
 }
 
 export default updateDescription;
